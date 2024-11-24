e1d2eb7384046dde748bea3e92dc375f30ee5670
Mayrzon02 <stephanolandauri@gmail.com>
2024-11-23 23:08:46 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+console.log("CrehanaWeb script cargado.");
+
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    alert('Formulario enviado correctamente');
+});
+
